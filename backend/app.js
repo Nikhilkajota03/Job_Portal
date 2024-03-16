6f1b62361681da6eb4dcf6a9a28f9461fc3ac0b8
NikhilTheSlayer <nikhilkajota9413750125@gmail.com>
2024-03-16 13:00:53 +0530

    change cors origin

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-bzxh.vercel.app",
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
